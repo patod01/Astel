1b613aeb99d64984d6790fcbe22135af96d96a69
patod01 <patocv2@hotmail.com>
2021-08-13 17:14:48 -0400

    First full test of functionality of web page.
    - Host seems to be working.
    - Added fundamental files of web page (html, css, js).
    - Added test for downloading files.

--- app/js/Astel.js
+++ app/js/Astel.js
@@ -1,1 +1,54 @@
+function tryRUT(rutIn) {
+     let RUTS = ['197420170', '189076959'];
+     let rut;
+
+     for (rut in RUTS) {
+          if (RUTS[rut] === rutIn) {
+               return true;
+          }
+     }
+     return false;
+}
+
 alert('I am a powerful lord')
+
+
+$('#pdf-button').click(function(){
+     console.log('wanna be my Queen?!!')
+     var linkSpawn = $('#pdf-spawn').html()
+     var rutInput = $('#rut-field').val()
+     console.log(linkSpawn)
+     console.log(rutInput)
+     console.log(typeof(rutInput))
+     if (linkSpawn === '' && tryRUT(rutInput)) {
+          $('#pdf-spawn').append(
+               '<br>\
+               <div>\
+                    Descargar --> <a id="pdf-link" href="./src/'
+                    + rutInput + '.pdf" download>PDF</a> <--\
+               </div>'
+          )
+          $('.pdf-container').append(
+               '<br><div>Para ingresar otro RUT,\
+               <a id="btn-recargar" href="">recargue</a>\
+               el sitio web.</div>'
+          )
+     }
+})
+
+
+$('p').click(function(){
+     console.log('hola')
+})
+
+$("#on-off").click(function(){
+     $("#contenedor").toggle()
+})
+
+$("#add").click(function(){
+     $("#contenedor").addClass("container")
+})
+
+$("#add").hover(function(){
+     console.log('nanii')
+})
